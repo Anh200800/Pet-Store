13c8a02564e50b8f654f229610a5be2e670ef249
Your Name <you@example.com>
2022-05-23 09:36:59 +0700

    header commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import Shop from './pages/Shop';
+function App() {
+  return (
+    <div className="App">
+      <Shop />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import img from "../assets/images/banner.png"
+const Banner = () => {
+  return (
+    
+    <div className="banner">
+
+    {/* <img src={img} alt="" className="banner-img" /> */}
+    {/* <div className="banner-content">
+    <h2 className="banner-title">Products</h2>
+    <p className="banner-desc">HOME / PRODUCTS</p>
+    </div> */}
+
+    </div>
+
+    
+  );
+}
+
+export default Banner;
